19aca8c2e9140a15eb6eec1eeb19fda6ee04719a
Alina <alina.boshkov@gmail.com>
2019-12-15 13:25:09 +0200

    updates

--- js/editor-controller.js
+++ js/editor-controller.js
@@ -29,41 +29,24 @@ function onCloseEditor() {
 }
 
 function onUpdateSize(diff) {
-    let meme = getCurrMeme();
-    let txtIdx = meme.selectedTxtIdx
-    let currSize = meme.txts[txtIdx].size
-    let newSize = currSize + diff;
-    meme.txts[txtIdx].size = newSize;
-    updateCurrMeme(meme)
+    updateSize(diff)
     drawMeme();
 }
 
 function onUpdateTxts() {
     let meme = getCurrMeme();
     let txt = document.querySelector('.txt-input').value
-    let txtIdx = meme.selectedTxtIdx
-
     if (meme.txts.length) {
-        meme.txts[txtIdx].line = txt
+        updateTxts(txt);
     } else {
         onAddLine()
     }
-
-    updateCurrMeme(meme)
     drawMeme();
 }
 
 function onSetTxtIdx() {
+    setTxtIdx()
     let meme = getCurrMeme();
-    let txtIdx = meme.selectedTxtIdx
-
-    if (txtIdx < meme.txts.length - 1) {
-        meme.selectedTxtIdx++;
-        updateCurrMeme(meme);
-    } else {
-        meme.selectedTxtIdx = 0
-    }
-
     if (meme.txts.length) {
         document.querySelector('.txt-input').value = meme.txts[meme.selectedTxtIdx].line;
     } else {
@@ -72,14 +55,9 @@ function onSetTxtIdx() {
 }
 
 function onAddLine() {
+    addLine()
     let meme = getCurrMeme();
-    meme.txts.push({
-        line: '', size: 40, fontFam: 'Impact', txtAlign: "start", fontColor: "black",
-        strokeColor: "white", x: 80, y: (gCanvas.height / 2)
-    })
-    meme.selectedTxtIdx = meme.txts.length - 1;
     document.querySelector('.txt-input').value = '';
-    updateCurrMeme(meme);
 
     if (meme.txts.length > 0) {
         let elDeleteBtn = document.querySelector(".delete-btn")
@@ -88,11 +66,9 @@ function onAddLine() {
 }
 
 function onDeleteLine() {
+    deleteLine();
     let meme = getCurrMeme();
-    let txtIdx = meme.selectedTxtIdx
-    meme.txts.splice(txtIdx, 1);
     document.querySelector('.txt-input').value = '';
-    updateCurrMeme(meme);
 
     if (meme.txts.length < 1) {
         let elDeleteBtn = document.querySelector(".delete-btn")
@@ -104,86 +80,29 @@ function onDeleteLine() {
 }
 
 function onUpdateHeight(diff) {
-    let meme = getCurrMeme();
-    let txtIdx = meme.selectedTxtIdx
-    let currHeight = meme.txts[txtIdx].y
-    meme.txts[txtIdx].y = currHeight + diff
-    updateCurrMeme(meme);
+    updateHeight(diff)
     drawMeme()
 }
 
 function onUpdateAlign(txtAlign) {
-    let meme = (getCurrMeme())
-    let text = meme.txts[meme.selectedTxtIdx]
-    switch (txtAlign) {
-        case "start":
-            text.txtAlign = "start"
-            text.x = 80;
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-        case "center":
-            text.txtAlign = "center"
-            text.x = gCanvas.width / 2;
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-
-        case "end":
-            text.txtAlign = "end"
-            text.x = gCanvas.width - 80;
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-    }
-}
-
-function onUpdateFontFamily(fontFamily) {
-    let meme = getCurrMeme()
-    let text = meme.txts[meme.selectedTxtIdx]
-
-    switch (fontFamily) {
-        case "impact":
-            text.fontFam = 'Impact'
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-        case "ariel":
-            text.fontFam = 'Ariel'
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-        case "helvetica":
-            text.fontFam = 'Helvetica'
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-        case "roboto":
-            text.fontFam = 'Roboto'
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-        case "calibri":
-            text.fontFam = 'Calibri'
-            updateCurrMeme(meme)
-            drawMeme()
-            break;
-    }
-}
-
-function onUpdateFontColor() {
-    let meme = getCurrMeme()
-    let fontColor = document.querySelector('.font-color').value
-    meme.txts[meme.selectedTxtIdx].fontColor = fontColor
-    updateCurrMeme(meme)
+    updateAlign(txtAlign)
     drawMeme()
 }
 
+function onUpdateFontFamily(fontFamily) {
+    updateFontFamily(fontFamily)
+    drawMeme()
+}
+
+function onUpdateFontColor() {
+    let fontColor = document.querySelector('.font-color').value
+    updateFontColor(fontColor)
+    drawMeme();
+}
+
 function onUpdateStrokeColor() {
-    let meme = getCurrMeme()
     let strokeColor = document.querySelector('.stroke-color').value
-    meme.txts[meme.selectedTxtIdx].strokeColor = strokeColor
-    updateCurrMeme(meme)
+    updateStrokeColor(strokeColor)
     drawMeme()
 }
 
@@ -197,20 +116,20 @@ function drawMeme() {
         gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
         for (let i = 0; i < meme.txts.length; i++) {
             let text = meme.txts[i]
-            drawText(text.line, text.fontFam, text.size, text.txtAlign, text.fontColor, text.strokeColor, text.x, text.y)
+            drawText(text)
         }
     };
     elImg.src = img.url
 }
 
-function drawText(txt, fontFam, size, txtAlign, fontColor, strokeColor, x, y) {
-    gCtx.font = `${size}px ${fontFam}`
-    gCtx.textAlign = txtAlign;
+function drawText(text) {
+    gCtx.font = `${text.size}px ${text.fontFam}`
+    gCtx.textAlign = text.txtAlign;
     gCtx.lineWidth = 2;
-    gCtx.fillStyle = fontColor;
-    gCtx.strokeStyle = strokeColor;
-    gCtx.fillText(txt, x, y);
-    gCtx.strokeText(txt, x, y);
+    gCtx.fillStyle = text.fontColor;
+    gCtx.strokeStyle = text.strokeColor;
+    gCtx.fillText(text.line, text.x, text.y);
+    gCtx.strokeText(text.line, text.x, text.y);
 }
 
 function downloadCanvas(elLink) {
--- js/service.js
+++ js/service.js
@@ -54,10 +54,106 @@ function createMeme(selectedImgId) {
     gCurrMeme = meme
 }
 
-function updateCurrMeme(meme) {
-    gCurrMeme = meme
-}
-
 function getCurrMeme() {
     return gCurrMeme
+}
+
+
+function updateSize(diff) {
+    let meme = gCurrMeme
+    let txtIdx = meme.selectedTxtIdx
+    let currSize = meme.txts[txtIdx].size
+    let newSize = currSize + diff;
+    meme.txts[txtIdx].size = newSize;
+}
+
+function updateTxts(txt) {
+    let meme = gCurrMeme
+    let txtIdx = meme.selectedTxtIdx
+    meme.txts[txtIdx].line = txt
+}
+
+function setTxtIdx() {
+    let meme = gCurrMeme
+    let txtIdx = meme.selectedTxtIdx
+    if (txtIdx < meme.txts.length - 1) {
+        meme.selectedTxtIdx++;
+    } else {
+        meme.selectedTxtIdx = 0
+    }
+}
+
+function addLine() {
+    let meme = gCurrMeme
+    meme.txts.push({
+        line: '', size: 40, fontFam: 'Impact', txtAlign: "start", fontColor: "black",
+        strokeColor: "white", x: 80, y: (gCanvas.height / 2)
+    })
+    meme.selectedTxtIdx = meme.txts.length - 1;
+}
+
+function deleteLine() {
+    let meme = gCurrMeme
+    let txtIdx = meme.selectedTxtIdx
+    meme.txts.splice(txtIdx, 1);
+}
+
+function updateHeight(diff) {
+    let meme = gCurrMeme;
+    let txtIdx = meme.selectedTxtIdx
+    let currHeight = meme.txts[txtIdx].y
+    meme.txts[txtIdx].y = currHeight + diff
+}
+
+function updateAlign(txtAlign) {
+    let meme = gCurrMeme;
+    let text = meme.txts[meme.selectedTxtIdx]
+    switch (txtAlign) {
+        case "start":
+            text.txtAlign = "start"
+            text.x = 80;
+            break;
+        case "center":
+            text.txtAlign = "center"
+            text.x = gCanvas.width / 2;
+            break;
+
+        case "end":
+            text.txtAlign = "end"
+            text.x = gCanvas.width - 80;
+            break;
+    }
+}
+
+function updateFontFamily(fontFamily){
+    let meme = gCurrMeme;
+    let text = meme.txts[meme.selectedTxtIdx]
+
+    switch (fontFamily) {
+        case "impact":
+            text.fontFam = 'Impact'
+            break;
+        case "ariel":
+            text.fontFam = 'Ariel'
+            break;
+        case "helvetica":
+            text.fontFam = 'Helvetica'
+            break;
+        case "roboto":
+            text.fontFam = 'Roboto'
+            break;
+        case "calibri":
+            text.fontFam = 'Calibri'
+            break;
+    }
+}
+
+function updateFontColor(fontColor){
+    let meme = gCurrMeme;
+    meme.txts[meme.selectedTxtIdx].fontColor = fontColor
+}
+
+function updateStrokeColor(strokeColor){
+    let meme = gCurrMeme;
+    meme.txts[meme.selectedTxtIdx].strokeColor = strokeColor
 }
